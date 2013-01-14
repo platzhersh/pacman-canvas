35450fd6a9d18b95c2be7a3088f522fcdbee18b9
Chregi Glatthard <chregi.glatthard@gmail.com>
2013-01-14 18:19:19 +0100

    check app-cache for updates

--- pacman-canvas.js
+++ pacman-canvas.js
@@ -362,6 +362,32 @@
 	}
 		var pacman = new pacman();
 	
+// Check if a new cache is available on page load.	 
+function checkAppCache() {
+window.addEventListener('load', function(e) 
+{
+	window.applicationCache.addEventListener('updateready', function(e) 
+	{
+		if (window.applicationCache.status == window.applicationCache.UPDATEREADY) {
+
+			// Browser downloaded a new app cache.
+			// Swap it in and reload the page to get the new hotness.
+			window.applicationCache.swapCache();
+			if (confirm('A new version of this site is available. Load it?')) {
+				window.location.reload();
+			}
+
+		} else {
+		// Manifest didn't changed. Nothing new to server.
+		}
+
+	}, false);
+
+	 
+
+}, false);
+
+}
 	
 	// Action starts here:
 	
@@ -391,7 +417,9 @@
 			})
 		
 		window.addEventListener('keydown',doKeyDown,true);
-
+		
+		checkAppCache();
+		
 		var canvas = $("#myCanvas").get(0);
 		var context = canvas.getContext("2d");
         
