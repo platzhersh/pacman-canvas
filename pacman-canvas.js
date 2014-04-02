19bc6c92573eb64f42d4aee6b191a47c5ba78615
Chregi Glatthard <chregi.glatthard@gmail.com>
2014-04-02 14:51:33 +0200

    requestAnimationFrame

--- pacman-canvas.js
+++ pacman-canvas.js
@@ -16,6 +16,7 @@
 	var joystick;
 	var context;
 	var game;
+	var canvas_walls, context_walls;
 	var inky, blinky, clyde, pinky;
 	
 	var mapConfig = 'data/map.json';
@@ -61,6 +62,7 @@
 	}
 	
 	function buildWall(context,gridX,gridY,width,height) {
+		console.log("BuildWall");
 		width = width*2-1;
 		height = height*2-1;
 		context.fillRect(pacman.radius/2+gridX*2*pacman.radius,pacman.radius/2+gridY*2*pacman.radius, width*pacman.radius, height*pacman.radius);
@@ -286,10 +288,13 @@
 		this.toGridPos = function (pixelPos) {
 			return ((pixelPos % 30)/30);
 		}
+
 	}
 
 	game = new Game();
-	
+
+
+
 	function Score() {
 		this.score = 0;
 		this.set = function(i) {
@@ -957,6 +962,76 @@
 	}
 	pacman.prototype = new Figure();
 	var pacman = new pacman();
+
+	/* ------------ Start Pre-Build Walls  ------------ */
+	canvas_walls = document.createElement('canvas');
+	canvas_walls.width = game.canvas.width;
+	canvas_walls.height = game.canvas.height;
+	context_walls = canvas_walls.getContext("2d");
+
+	context_walls.fillStyle = "Blue";
+	context_walls.strokeStyle = "Blue";
+	
+	//horizontal outer
+	buildWall(context_walls,0,0,18,1);
+	buildWall(context_walls,0,12,18,1);
+	
+	// vertical outer
+	buildWall(context_walls,0,0,1,6);
+	buildWall(context_walls,0,7,1,6);
+	buildWall(context_walls,17,0,1,6);
+	buildWall(context_walls,17,7,1,6);
+	
+	// ghost base
+	buildWall(context_walls,7,4,1,1);
+	buildWall(context_walls,6,5,1,2);
+	buildWall(context_walls,10,4,1,1);
+	buildWall(context_walls,11,5,1,2);
+	buildWall(context_walls,6,6,6,1);
+	
+	// ghost base door
+	context_walls.fillRect(8*2*pacman.radius,pacman.radius/2+4*2*pacman.radius+5, 4*pacman.radius, 1);
+	
+	// single blocks
+	buildWall(context_walls,4,0,1,2);
+	buildWall(context_walls,13,0,1,2);
+	
+	buildWall(context_walls,2,2,1,2);
+	buildWall(context_walls,6,2,2,1);
+	buildWall(context_walls,15,2,1,2);
+	buildWall(context_walls,10,2,2,1);
+	
+	buildWall(context_walls,2,3,2,1);
+	buildWall(context_walls,14,3,2,1);
+	buildWall(context_walls,5,3,1,1);
+	buildWall(context_walls,12,3,1,1);
+	buildWall(context_walls,3,3,1,3);
+	buildWall(context_walls,14,3,1,3);
+	
+	buildWall(context_walls,3,4,1,1);
+	buildWall(context_walls,14,4,1,1);
+	
+	buildWall(context_walls,0,5,2,1);
+	buildWall(context_walls,3,5,2,1);
+	buildWall(context_walls,16,5,2,1);
+	buildWall(context_walls,13,5,2,1);
+	
+	buildWall(context_walls,0,7,2,2);
+	buildWall(context_walls,16,7,2,2);
+	buildWall(context_walls,3,7,2,2);
+	buildWall(context_walls,13,7,2,2);
+	
+	buildWall(context_walls,4,8,2,2);
+	buildWall(context_walls,12,8,2,2);
+	buildWall(context_walls,5,8,3,1);
+	buildWall(context_walls,10,8,3,1);
+	
+	buildWall(context_walls,2,10,1,1);
+	buildWall(context_walls,15,10,1,1);
+	buildWall(context_walls,7,10,4,1);
+	buildWall(context_walls,4,11,2,2);
+	buildWall(context_walls,12,11,2,2);
+	/* ------------ End Pre-Build Walls  ------------ */
 	
 // Check if a new cache is available on page load.	 
 function checkAppCache() {
@@ -1130,6 +1205,7 @@ function checkAppCache() {
 		{
 			//context.save()
 			
+
 			// Refresh Score
 			game.score.refresh(".score");
 						
@@ -1154,75 +1230,9 @@ function checkAppCache() {
 			});
 			context.fill();
 			
-			//context.beginPath();
-			context.fillStyle = "Blue";
-			context.strokeStyle = "Blue";
+			// Walls
+			context.drawImage(canvas_walls, 0, 0);
 			
-			//horizontal outer
-			buildWall(context,0,0,18,1);
-			buildWall(context,0,12,18,1);
-			
-			// vertical outer
-			buildWall(context,0,0,1,6);
-			buildWall(context,0,7,1,6);
-			buildWall(context,17,0,1,6);
-			buildWall(context,17,7,1,6);
-			
-			// ghost base
-			buildWall(context,7,4,1,1);
-			buildWall(context,6,5,1,2);
-			buildWall(context,10,4,1,1);
-			buildWall(context,11,5,1,2);
-			buildWall(context,6,6,6,1);
-			
-			// ghost base door
-			context.fillRect(8*2*pacman.radius,pacman.radius/2+4*2*pacman.radius+5, 4*pacman.radius, 1);
-			
-			// single blocks
-			buildWall(context,4,0,1,2);
-			buildWall(context,13,0,1,2);
-			
-			buildWall(context,2,2,1,2);
-			buildWall(context,6,2,2,1);
-			buildWall(context,15,2,1,2);
-			buildWall(context,10,2,2,1);
-			
-			buildWall(context,2,3,2,1);
-			buildWall(context,14,3,2,1);
-			buildWall(context,5,3,1,1);
-			buildWall(context,12,3,1,1);
-			buildWall(context,3,3,1,3);
-			buildWall(context,14,3,1,3);
-			
-			buildWall(context,3,4,1,1);
-			buildWall(context,14,4,1,1);
-			
-			buildWall(context,0,5,2,1);
-			buildWall(context,3,5,2,1);
-			buildWall(context,16,5,2,1);
-			buildWall(context,13,5,2,1);
-			
-			buildWall(context,0,7,2,2);
-			buildWall(context,16,7,2,2);
-			buildWall(context,3,7,2,2);
-			buildWall(context,13,7,2,2);
-			
-			buildWall(context,4,8,2,2);
-			buildWall(context,12,8,2,2);
-			buildWall(context,5,8,3,1);
-			buildWall(context,10,8,3,1);
-			
-			buildWall(context,2,10,1,1);
-			buildWall(context,15,10,1,1);
-			buildWall(context,7,10,4,1);
-			buildWall(context,4,11,2,2);
-			buildWall(context,12,11,2,2);
-			/*context.moveTo(pacman.radius/2,pacman.radius/2);
-			context.lineTo(pacman.radius + pacman.radius/2,pacman.radius/2);
-			context.lineTo(pacman.radius + pacman.radius/2,pacman.radius + pacman.radius/2);
-			context.lineTo(pacman.radius/2,pacman.radius + pacman.radius/2);
-			context.closePath();*/
-			//context.fill();
 			
 			if (game.running == true) {
 				// Ghosts
@@ -1314,7 +1324,8 @@ function checkAppCache() {
 			game.check();
 			
 			
-			setTimeout(animationLoop, game.refreshRate);
+			requestAnimationFrame(animationLoop);
+			//setTimeout(animationLoop, game.refreshRate);
 			
 			
 		}
