ae09c0663f80f1da2aba454e64141b8c379c6aa7
platzhersh <chregi.glatthard@gmail.com>
2016-05-24 15:14:48 +0200

    implement vibration api

--- pacman-canvas.js
+++ pacman-canvas.js
@@ -1327,18 +1327,22 @@ function checkAppCache() {
 		// Mobile Control Buttons
 		$(document).on('touchend mousedown','#up',function(event) {
 		    event.preventDefault();
+		    window.navigator.vibrate(200);
 			pacman.directionWatcher.set(up);
 		});
 		$(document).on('touchend mousedown','#down',function(event) {
 		    event.preventDefault();
+		    window.navigator.vibrate(200);
 			pacman.directionWatcher.set(down);
 		});
 		$(document).on('touchend mousedown','#left',function(event) {
 		    event.preventDefault();
+		    window.navigator.vibrate(200);
 			pacman.directionWatcher.set(left);
 		});
 		$(document).on('touchend mousedown','#right',function(event) {
 		    event.preventDefault();
+		    window.navigator.vibrate(200);
 			pacman.directionWatcher.set(right);
 		});
 		
