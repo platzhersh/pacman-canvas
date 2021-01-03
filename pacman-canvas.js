afb420505271f94a3f2dfebbf379defe663b2e32
Chregi <chregi.glatthard@gmail.com>
2021-01-03 21:27:23 +0100

    Develop (#26)
    * Update db-handler.php
    
    * Revert "Update db-handler.php"
    
    This reverts commit 37ce2579575a23220041483485cb88db089955ea.
    
    * cleanup
    
    * cleanup
    
    * score validation
    
    * rebase on master
    
    * remove resetHighscore endpoint

--- pacman-canvas.js
+++ pacman-canvas.js
@@ -652,7 +652,7 @@ function geronimo() {
 	var up = new Direction("up", 1.75, 1.25, 0, -1);		// UP
 	var left = new Direction("left", 1.25, 0.75, -1, 0);	// LEFT
 	var down = new Direction("down", 0.75, 0.25, 0, 1);		// DOWN
-	var right = new Direction("right", 0.25, 1.75, 1, 0);	// 
+	var right = new Direction("right", 0.25, 1.75, 1, 0);	// RIGHT
 	/*var directions = [{},{},{},{}];
 	directions[0] = up;
 	directions[1] = down;
